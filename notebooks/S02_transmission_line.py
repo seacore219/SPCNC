f7872eec901c3e3a92e067d50729d0f628fd36bb
seacore219 <seacore219@outlook.com>
2026-09-11 01:02:50 -0500

    transmission line nb and py, divider nb

---
+++ notebooks/S02_transmission_line.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from S01_pulser import pulser1, pulser2
+
+Td = 5e-9   # T3/T4 propagation delay
+Z0 = 50     # T3/T4 characteristic impedance
+RP = 50     # R4/R11 matched termination -- equals Z0 by design
+
+
